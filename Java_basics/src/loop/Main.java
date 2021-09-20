d0167b850124de049ef1eeb3322acb8dd31a69ae
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2021-09-20 11:55:52 +0600

    Create Main.java

---
+++ Java_basics/src/loop/Main.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=56,b=56;
+		
+		for(int i=0;i<100;i++) {
+			System.out.println("Hello world");
+		}
+	}
+
+}
