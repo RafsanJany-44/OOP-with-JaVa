a7c3d00308e858f91e536a1764a44aa3dfd7005d
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2021-09-21 20:17:23 +0600

    Update Main.java

--- Java_basics/src/methods_/Main.java
+++ Java_basics/src/methods_/Main.java
@@ -2,6 +2,7 @@ package methods_;
 
 public class Main {
 	
+	
 	static int factorial(int num) {
 		int fact=1;
 		for(int i=1; i<=num; i++) {
@@ -9,12 +10,37 @@ public class Main {
 		}
 		return fact;
 	}
-
+	
+	
+	static float add(float a, float b) {
+		return a+b;
+	}
+	
+	static float sub(float a, float b) {
+		return a-b;
+	}
+	
+	static float mul(float a, float b) {
+		return a*b;
+	}
+	static float div(float a, float b) {
+		return a/b;
+	}
+	
+	
 	public static void main(String[] args) {
 		int b;
 		b=factorial(6);
 		System.out.println(b);
+		System.out.println(add(5,6));
+		System.out.println(sub(5,6));
+
+		System.out.println(mul(5,6));
+
+		System.out.println(div(5,6));
+
 	}
+	
 	
 	
 	
