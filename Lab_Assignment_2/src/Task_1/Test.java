bf911f33c0efe7694afd43ee36c7b56059d82b38
Rafsan Jany, 170042044 <59754354+RafsanJany-44@users.noreply.github.com>
2022-01-06 11:19:22 +0600

    Update Test.java

--- Lab_Assignment_2/src/Task_1/Test.java
+++ Lab_Assignment_2/src/Task_1/Test.java
@@ -1,21 +1,24 @@
 package Task_1;
+import java.util.*;
 public class Test {
 
 	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
 		
+		int num=input.nextInt();
         try {
-                testException(4);
+                testException(num);
         } catch (Exception_1 e) {
                 System.out.println(e);
         }
 	}
-        public static void testException(int n)throws Exception_1,Exception_2,Exception_3 {
+        public static void testException(int sen)throws Exception_1,Exception_2,Exception_3 {
            
-            if(n<10) 
+            if(sen==1) 
             {
             	throw new Exception_1("Throwing Exception 1");
             }
-            else if(n<30) 
+            else if(sen==2) 
             {
             	throw new Exception_2("Throwing Exception 2");
             }
