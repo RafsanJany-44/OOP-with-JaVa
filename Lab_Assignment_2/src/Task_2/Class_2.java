ba0e9eb96d5a76ee2bc5629ffbed5e9b0ec96fd3
Rafsan Jany, 170042044 <59754354+RafsanJany-44@users.noreply.github.com>
2022-01-06 11:33:23 +0600

    up

--- Lab_Assignment_2/src/Task_2/Class_2.java
+++
@@ -1,8 +0,0 @@
-package Task_2;
-
-public class Class_2 extends Class_1{
-	 @Override
-     public void method1() throws Exception {
-             super.method1();
-     }
-}
--- Lab_Assignment_2/src/Task_2/Class_3.java
+++
@@ -1,9 +0,0 @@
-package Task_2;
-
-public class Class_3 extends Class_2{
-	@Override
-    public void method1() throws Exception {
-            super.method1();
-    }
-
-}
